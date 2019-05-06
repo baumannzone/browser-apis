a35bebfedca23520b73b94c112272bb9ab51771d
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-06 22:21:22 +0200

    Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,12 @@
         <h1 class="mt-5">¿Sabías que tu navegador puede hablar?</h1>
         <h2 class="mt-0 text-muted">Y otras Web APIs</h2>
       </div>
-      <p class="text-right font-weight-bold mt-5">
+      <p class="text-right font-weight-bold mt-5 mb-0">
         Jorge Baumann · <a href="https://twitter.com/baumannzone">@baumannzone</a>
+      </p>
+
+      <p class="text-right text-muted mt-0">
+        <small>#io19Extended</small>
       </p>
     </MainBlock>
   </div>
