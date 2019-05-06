6c8be3d3d7b0c14a7fe4ef1777391ea980fcd0e0
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-06 20:00:52 +0200

    Routes

--- src/router.js
+++ src/router.js
@@ -1,20 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+const routerOptions = [
+  {path: '/', name: 'Home', viewName: 'Home'},
+  {path: '/speech', name: 'Speech', viewName: 'Speech'},
+  {path: '/speech-recognition', name: 'SpeechRecognition', viewName: 'SpeechRecognition'},
+  {path: '/battery', name: 'Battery', viewName: 'Battery'},
+  {path: '/vibration', name: 'Vibration', viewName: 'Vibration'},
+  {path: '/geolocation', name: 'Geolocation', viewName: 'Geolocation'},
+  {path: '/notifications', name: 'Notifications', viewName: 'Notifications'},
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/views/${route.viewName}.vue`)
+  }
+})
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('./views/Speech.vue')
-    }
-  ]
+const router = new Router({
+  routes
 })
+
+export default router
