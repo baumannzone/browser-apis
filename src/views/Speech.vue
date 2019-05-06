3a1d55b09749aa75c742e8990ba47a462cd72c93
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-07 00:48:08 +0200

    Demo 2

--- src/views/Speech.vue
+++ src/views/Speech.vue
@@ -2,13 +2,15 @@
   <div class="speech">
     <MainBlock title="Speech synthesis">
       <p class="text-muted lead text-center">aka text-to-speech</p>
+
       <hr>
+
       <b-card class="mb-2">
         <h4>#1</h4>
         <b-form @submit="demo1">
 
           <b-form-group label="Text:" label-for="inputDemo1">
-            <b-form-input id="inputDemo1" v-model="inputDemo1" placeholder="Type something.."></b-form-input>
+            <b-form-input id="inputDemo1" v-model="form1.input" placeholder="Type something.."></b-form-input>
           </b-form-group>
 
           <b-button type="submit" variant="primary" size="sm">Submit</b-button>
@@ -34,7 +36,20 @@
         <b-form @submit="demo2">
 
           <b-form-group label="Text:" label-for="inputDemo2">
-            <b-form-input id="inputDemo2" v-model="inputDemo2" placeholder="Type something cool.."></b-form-input>
+            <b-form-input id="inputDemo2" v-model="form2.input" placeholder="Type something cool.."></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Pitch (Tone)" label-for="rangePitchDemo2">
+            <b-form-input id="rangePitchDemo2" v-model="form2.pitch" type="range" min="0" max="2" step="0.05">
+            </b-form-input>
+            <small>Value: {{form2.pitch}}</small>
+          </b-form-group>
+
+          <!-- speed -->
+          <b-form-group label="Rate (Speed)" label-for="rangeRateDemo2">
+            <b-form-input id="rangeRateDemo2" v-model="form2.rate" type="range" min="0.46" max="3.6" step="0.01">
+            </b-form-input>
+            <small>Value: {{form2.rate}}</small>
           </b-form-group>
 
           <b-button type="submit" variant="primary" size="sm">Submit</b-button>
@@ -46,9 +61,11 @@
         <div class="mt-2">
           <b-collapse id="demo2">
             <highlight-code lang="javascript">
-              const text = 'Hola, soy Jorge Baumann'
+              cconst text = 'Hola, soy Jorge Baumann y mi perro se llama Rambo'
               const synth = window.speechSynthesis
               const utterThis = new SpeechSynthesisUtterance(text)
+              utterThis.pitch = 1.5
+              utterThis.rate = 1.5
               synth.speak(utterThis)
             </highlight-code>
           </b-collapse>
@@ -69,14 +86,37 @@ export default {
   },
   data () {
     return {
-      inputDemo1: '',
-      showDemo1: false
+      voices: [],
+      voiceNames: [],
+      selectedVoiceName: 0,
+      form1: {
+        input: ''
+      },
+      form2: {
+        input: '',
+        pitch: 1,
+        rate: 1
+      }
+    }
+  },
+  created () {
+    // Check support for speech API
+    if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = this.populateVoiceList
+    } else {
+      console.warn('Nope!')
     }
   },
   methods: {
+    populateVoiceList () {
+      this.voices = window.speechSynthesis.getVoices()
+      this.voiceNames = this.voices.map((v) => {
+        return { name: v.name, lang: v.lang }
+      })
+    },
     demo1 (ev) {
       ev.preventDefault()
-      const text = this.inputDemo1
+      const text = this.demo1.input
       const synth = window.speechSynthesis
       const utterThis = new SpeechSynthesisUtterance(text)
       synth.speak(utterThis)
