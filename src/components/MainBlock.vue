4281ac947c4b8b2bcad59d7f88c7e219aceef82a
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-06 22:44:30 +0200

    Speech

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="MainBlock">
-    <h1 v-if="title"> {{ title }} </h1>
+    <h1 class="text-center" v-if="title"> {{ title }} </h1>
+    <h3 class="text-center" v-if="subtitle"> {{ subtitle }}</h3>
     <slot></slot>
   </div>
 </template>
@@ -10,6 +11,10 @@ export default {
   name: 'HelloWorld',
   props: {
     title: {
+      type: String,
+      required: false
+    },
+    subtitle: {
       type: String,
       required: false
     }
--- src/views/Speech.vue
+++ src/views/Speech.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="speech">
+    <MainBlock title="Speech synthesis">
+      <p class="text-muted lead text-center">aka text-to-speech</p>
+      <hr>
+      <div>
+        <h4>#1</h4>
+        <b-form @submit="demo1">
+
+          <b-form-group label="Text:" label-for="inputDemo1">
+            <b-form-input id="inputDemo1" v-model="inputDemo1" placeholder="Type something.."></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
+        <hr>
+      </div>
+
+    </MainBlock>
   </div>
 </template>
+
+<script>
+import MainBlock from '@/components/MainBlock.vue'
+
+export default {
+  name: 'Speech',
+  components: {
+    MainBlock
+  },
+  data () {
+    return {
+      inputDemo1: '',
+      showDemo1: false
+    }
+  },
+  methods: {
+    demo1 (ev) {
+      ev.preventDefault()
+      const text = this.inputDemo1
+      const synth = window.speechSynthesis
+      const utterThis = new SpeechSynthesisUtterance(text)
+      synth.speak(utterThis)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
