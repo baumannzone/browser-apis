cda0c60e8ec6787d09956ab6814f8f719b4703ff
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-07 12:49:56 +0200

    Geolocation

--- src/views/Geolocation.vue
+++ src/views/Geolocation.vue
@@ -6,11 +6,21 @@
         <h4>#1</h4>
         <hr>
         <div class="data">
-          <h4>Coordinates</h4>
-          <p v-if="isLoading" class="lead">Loading...</p>
-          <template v-else>
-            datasss
-          </template>
+          <b-button variant="primary" size="sm" @click="getPosition">Get Position</b-button>
+
+          <div v-if="hasCoords">
+            <h4 class="mt-2">Position</h4>
+            <p class="lead">
+              Latitude is: <b-badge>{{ coords.lat}}</b-badge>
+              <br>
+              Longitude is: <b-badge>{{ coords.long}}</b-badge>
+            </p>
+
+          </div>
+
+          <p v-if="isGeoAvailable === false">
+            Geolocation not available
+          </p>
 
         </div>
 
@@ -58,47 +68,46 @@ export default {
   },
   data () {
     return {
-      position: null,
-      isLoading: true
+      coords: {
+        lat: '',
+        long: ''
+      },
+      isGeoAvailable: true
     }
   },
-  computed: {},
-  created () {
-    this.demo1()
+  computed: {
+    hasCoords () {
+      return this.coords.lat !== '' && this.coords.long !== ''
+    }
   },
   methods: {
-    demo1 () {
+    getPosition () {
       if ('geolocation' in navigator) {
-        /* geolocalización disponible */
-        console.log('Loading position...')
-        navigator.geolocation.getCurrentPosition(position => {
-          console.log('Position:')
-          console.log(position.coords)
-          const latitude = position.coords.latitude
-          const longitude = position.coords.longitude
-          // output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>'
-          const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=8e456d1b8c004a1384e71d9a6c97e94f`
-          fetch(url)
-            .then(response => response.json())
-            .then((res) => {
-              console.log('External · Geo API:')
-              console.log(res.results[0].formatted)
-              console.log(res.results[0].components)
+        navigator.geolocation
+          .getCurrentPosition((position) => {
+            const lat = position.coords.latitude
+            const long = position.coords.longitude
+            this.coords.lat = lat
+            this.coords.long = long
+            const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=8e456d1b8c004a1384e71d9a6c97e94f`
+            fetch(url)
+              .then(response => response.json())
+              .then((res) => {
+                console.log('External · Geo API:')
+                console.log(res.results[0].formatted)
+                console.log(res.results[0].components)
               // console.log(res.results[0].components.country)
               // console.log(res.results[0].components.state)
               // console.log(res.results[0].components.county)
               // console.log(res.results[0].components.city)
-            })
-        }, error => console.error(error))
+              })
+          }, (error) => {
+            console.error(error)
+          })
       } else {
-        console.log('GEO no disponible')
-        /* geolocalización NO disponible */
+        this.isGeoAvailable = false
       }
     }
-  },
-  beforeRouteLeave (to, from, next) {
-    // Clear battery event listeners
-    next()
   }
 }
 </script>
