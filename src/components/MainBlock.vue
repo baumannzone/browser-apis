8e6345298c69ed987acc3f502ba41932584ef5e5
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-07 16:04:26 +0200

    Orientation

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="MainBlock bg-light p-5">
+  <div class="MainBlock bg-light p-md-5 p-2">
     <h1 v-if="title" class="text-center"> {{ title }} </h1>
     <p v-if="subtitle" class="text-muted lead text-center">{{ subtitle }}</p>
     <hr class="my-5">
