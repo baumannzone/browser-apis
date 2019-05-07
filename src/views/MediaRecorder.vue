127d3443756c4897101bc60e2e767405e2614db6
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-07 17:38:50 +0200

    Last?

--- src/views/MediaRecorder.vue
+++ src/views/MediaRecorder.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
+  <div class="text-center">
+    <hr>
     <h1>WIP</h1>
+    <p>MediaStream Recording API provides functionality to easily record media (video · audio)</p>
+    <hr>
   </div>
 </template>
--- src/views/SpeechRecognition.vue
+++ src/views/SpeechRecognition.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
+  <div class="text-center">
+    <hr>
     <h1>WIP</h1>
+    <pre>Voice to text</pre>
+    <hr>
   </div>
 </template>
