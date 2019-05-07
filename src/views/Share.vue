ee7ec1cd6dc53fe96e45b17799ba21b8abf455f4
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-05-07 14:37:02 +0200

    Share

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -1,5 +1,92 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="share-api">
+    <!-- Sintesis de voz -->
+    <MainBlock title="Web Share API">
+
+      <p class="lead text-muted">
+        The <code>Navigator.share()</code> method invokes the native sharing mechanism of the device as part of the Web Share API. If the Web Share API is unsupported, then this method will be undefined.
+      </p>
+
+      <b-card class="mb-5">
+        <h4>#1</h4>
+        <hr>
+        <div class="data">
+          <b-button variant="primary" size="sm" @click="demo1">Share</b-button>
+          <br>
+          <p v-if="message">{{ message }}</p>
+        </div>
+
+        <div class="code">
+          <div class="d-flex justify-content-end">
+            <b-button v-b-toggle="'demo2'" variant="outline-secondary" size="sm">Code</b-button>
+          </div>
+          <div class="mt-2">
+            <b-collapse id="demo2">
+              <highlight-code lang="javascript">
+                navigator.geolocation
+                .getCurrentPosition((position) => {
+                const lat = position.coords.latitude
+                const long = position.coords.longitude
+                const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=1234567890`
+                fetch(url)
+                .then(response => response.json())
+                .then((res) => {
+                // Handle response
+                })
+                }, (error) => {
+                // Handle error
+                })
+              </highlight-code>
+            </b-collapse>
+          </div>
+        </div>
+      </b-card>
+
+    </MainBlock>
   </div>
 </template>
+
+<script>
+import MainBlock from '@/components/MainBlock.vue'
+
+export default {
+  name: 'Speech',
+  components: {
+    MainBlock
+  },
+  data () {
+    return {
+      message: '',
+      error: ''
+    }
+  },
+  methods: {
+    demo1 () {
+      console.log('sharing...')
+      this.message = ''
+      if (navigator.share) {
+        const data = {
+          title: 'Baumannzone',
+          text: 'Hey, look at this awesome twitter guy! 🙊',
+          url: 'https://twitter.com/baumannzone'
+        }
+        navigator.share(data)
+          .then(() => {
+            this.message = 'Successful share'
+            console.log('Successful share')
+          })
+          .catch((error) => {
+            this.message = error
+            console.log('Error sharing', error)
+          })
+      } else {
+        this.error = 'You can not share! ☹️'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
