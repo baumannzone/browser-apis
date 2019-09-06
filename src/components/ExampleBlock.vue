32ec696c14a8fbac9551b8fdf1ff441aed9187a3
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-09-06 11:43:24 +0200

    Speech

---
+++ src/components/ExampleBlock.vue
@@ -0,0 +1,8 @@
+<template>
+  <h4>#1</h4>
+</template>
+<script>
+  export default {
+    name: 'ExampleBlock'
+  }
+</script>
--- src/views/Speech/Index.vue
+++ src/views/Speech/Index.vue
@@ -5,141 +5,114 @@
 
       <DefinitionBlock :description="description"/>
 
-      <!-- Ejemplo -->
-      <b-card class="mb-5">
-        <h4>#1</h4>
-        <b-form @submit="demo1">
+      <ExampleBlock exampleNumber="1">
+        <template v-slot:code>
+          <b-form @submit="demo1">
 
-          <b-form-group label="Text:" label-for="inputDemo1">
-            <b-form-input id="inputDemo1" v-model="form1.input" placeholder="Type something.." autocomplete="off"></b-form-input>
-          </b-form-group>
+            <b-form-group label="Text:" label-for="inputDemo1">
+              <b-form-input id="inputDemo1" v-model="form1.input" placeholder="Type something.." autocomplete="off"></b-form-input>
+            </b-form-group>
 
-          <b-button type="submit" variant="primary" size="sm">Submit</b-button>
-        </b-form>
-        <hr>
-        <div class="d-flex justify-content-end">
-          <b-button v-b-toggle="'demo1'" variant="outline-secondary" size="sm">Code</b-button>
-        </div>
-        <div class="mt-2">
-          <b-collapse id="demo1">
-            <highlight-code lang="javascript">
-              const text = 'Hola, soy Jorge Baumann'
-              const synth = window.speechSynthesis
-              const utterThis = new SpeechSynthesisUtterance(text)
-              synth.speak(utterThis)
-            </highlight-code>
-          </b-collapse>
-        </div>
-      </b-card>
+            <b-button type="submit" variant="primary" size="sm">Submit</b-button>
+          </b-form>
+        </template>
+        <template v-slot:example-code>
+          const text = 'Hola, soy Jorge Baumann. @baumannzone en twitter'
+          const synth = window.speechSynthesis
+          const utterThis = new SpeechSynthesisUtterance(text)
+          synth.speak(utterThis)
+        </template>
+      </ExampleBlock>
 
-      <!-- Ejemplo -->
-      <b-card class="mb-5">
-        <h4>#2</h4>
-        <b-form @submit="demo2">
+      <ExampleBlock exampleNumber="2">
+        <template v-slot:code>
+          <b-form @submit="demo2">
 
-          <b-form-group label="Text:" label-for="inputDemo2">
-            <b-form-input id="inputDemo2" v-model="form2.input" placeholder="Type something cool.." autocomplete="off"></b-form-input>
-          </b-form-group>
+            <b-form-group label="Text:" label-for="inputDemo2">
+              <b-form-input id="inputDemo2" v-model="form2.input" placeholder="Type something cool.." autocomplete="off"></b-form-input>
+            </b-form-group>
 
-          <b-form-group label="Pitch (Tone)" label-for="rangePitchDemo2">
-            <b-form-input id="rangePitchDemo2" v-model="form2.pitch" type="range" min="0" max="2" step="0.05">
-            </b-form-input>
-            <small>Value: {{form2.pitch}}</small>
-          </b-form-group>
+            <b-form-group label="Pitch (Tone)" label-for="rangePitchDemo2">
+              <b-form-input id="rangePitchDemo2" v-model="form2.pitch" type="range" min="0" max="2" step="0.05">
+              </b-form-input>
+              <small>Value: {{form2.pitch}}</small>
+            </b-form-group>
 
-          <!-- speed -->
-          <b-form-group label="Rate (Speed)" label-for="rangeRateDemo2">
-            <b-form-input id="rangeRateDemo2" v-model="form2.rate" type="range" min="0.46" max="3.6" step="0.01">
-            </b-form-input>
-            <small>Value: {{form2.rate}}</small>
-          </b-form-group>
+            <!-- speed -->
+            <b-form-group label="Rate (Speed)" label-for="rangeRateDemo2">
+              <b-form-input id="rangeRateDemo2" v-model="form2.rate" type="range" min="0.46" max="3.6" step="0.01">
+              </b-form-input>
+              <small>Value: {{form2.rate}}</small>
+            </b-form-group>
 
-          <b-button type="submit" variant="primary" size="sm">Submit</b-button>
-        </b-form>
-        <hr>
-        <div class="d-flex justify-content-end">
-          <b-button v-b-toggle="'demo2'" variant="outline-secondary" size="sm">Code</b-button>
-        </div>
-        <div class="mt-2">
-          <b-collapse id="demo2">
-            <highlight-code lang="javascript">
-              cconst text = 'Hola, soy Jorge Baumann y mi perro se llama Rambo'
-              const synth = window.speechSynthesis
-              const utterThis = new SpeechSynthesisUtterance(text)
-              utterThis.pitch = 1.5
-              utterThis.rate = 1.5
-              synth.speak(utterThis)
-            </highlight-code>
-          </b-collapse>
-        </div>
-      </b-card>
+            <b-button type="submit" variant="primary" size="sm">Submit</b-button>
+          </b-form>
+        </template>
+        <template v-slot:example-code>
+          const text = 'Hola, soy Jorge Baumann y mi perro se llama Rambo'
+          const synth = window.speechSynthesis
+          const utterThis = new SpeechSynthesisUtterance(text)
+          utterThis.pitch = 1.5
+          utterThis.rate = 1.5
+          synth.speak(utterThis)
+        </template>
+      </ExampleBlock>
 
-      <!-- Ejemplo -->
-      <b-card class="mb-5">
-        <h4>#3</h4>
-        <b-form @submit="demo3">
+      <ExampleBlock exampleNumber="3">
+        <template v-slot:code>
+          <b-form @submit="demo3">
 
-          <b-form-group label="Text:" label-for="inputDemo3">
-            <b-form-input id="inputDemo3" v-model="form3.input" placeholder="🦄 Type something awesome.." autocomplete="off"></b-form-input>
-          </b-form-group>
+            <b-form-group label="Text:" label-for="inputDemo3">
+              <b-form-input id="inputDemo3" v-model="form3.input" placeholder="🦄 Type something awesome.." autocomplete="off"></b-form-input>
+            </b-form-group>
 
-          <b-form-group label="Voices:">
-            <b-form-select v-model="selectedVoiceName" :options="voiceNames"></b-form-select>
-          </b-form-group>
+            <b-form-group label="Voices:">
+              <b-form-select v-model="selectedVoiceName" :options="voiceNames"></b-form-select>
+            </b-form-group>
 
-          <b-form-group label="Pitch (Tone)" label-for="rangePitchDemo3">
-            <b-form-input id="rangePitchDemo3" v-model="form3.pitch" type="range" min="0" max="2" step="0.05">
-            </b-form-input>
-            <small>Value: {{form3.pitch}}</small>
-          </b-form-group>
+            <b-form-group label="Pitch (Tone)" label-for="rangePitchDemo3">
+              <b-form-input id="rangePitchDemo3" v-model="form3.pitch" type="range" min="0" max="2" step="0.05">
+              </b-form-input>
+              <small>Value: {{form3.pitch}}</small>
+            </b-form-group>
 
-          <b-form-group label="Rate (Speed)" label-for="rangeRateDemo3">
-            <b-form-input id="rangeRateDemo3" v-model="form3.rate" type="range" min="0.46" max="3.6" step="0.01">
-            </b-form-input>
-            <small>Value: {{form3.rate}}</small>
-          </b-form-group>
+            <b-form-group label="Rate (Speed)" label-for="rangeRateDemo3">
+              <b-form-input id="rangeRateDemo3" v-model="form3.rate" type="range" min="0.46" max="3.6" step="0.01">
+              </b-form-input>
+              <small>Value: {{form3.rate}}</small>
+            </b-form-group>
 
-          <b-button type="submit" variant="primary" size="sm">Submit</b-button>
-        </b-form>
-        <hr>
-        <div class="d-flex justify-content-end">
-          <b-button v-b-toggle="'demo3'" variant="outline-secondary" size="sm">Code</b-button>
-        </div>
-        <div class="mt-2">
-          <b-collapse id="demo3">
+            <b-button type="submit" variant="primary" size="sm">Submit</b-button>
+          </b-form>
+        </template>
 
-            <highlight-code lang="javascript">
-              // Check support for speech API
-              if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
-                speechSynthesis.onvoiceschanged = this.populateVoiceList
-              } else {
-                console.warn('Nope!')
-              }
-            </highlight-code>
+        <template v-slot:example-code>
+          // Check support for speech API
+          if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+            speechSynthesis.onvoiceschanged = this.populateVoiceList
+          } else {
+            console.warn('Nope!')
+          }
 
-            <highlight-code lang="javascript">
+          // getVoices
+          speechSynthesis.onvoiceschanged = () => {
+            const text = 'Hola, soy Jorge Baumann y mi perro se llama Rambo 🐶'
+            const synth = speechSynthesis
+            const voices = synth.getVoices()
+            const utterThis = new SpeechSynthesisUtterance(text)
+            utterThis.voice = voices.find(v => v.name === 'Jorge')
+            utterThis.pitch = 1.5
+            utterThis.rate = 2
+            synth.speak(utterThis)
+          }
+        </template>
 
-              // getVoices
-              speechSynthesis.onvoiceschanged = () => {
-                const text = 'Hola, soy Jorge Baumann y mi perro se llama Rambo 🐶'
-                const synth = speechSynthesis
-                const voices = synth.getVoices()
-                const utterThis = new SpeechSynthesisUtterance(text)
-                utterThis.voice = voices.find(v => v.name === 'Jorge')
-                utterThis.pitch = 1.5
-                utterThis.rate = 2
-                synth.speak(utterThis)
-              }
-
-            </highlight-code>
-
-            <p class="lead">
-              More info & slides <a class="font-weight-bold" href="https://github.com/baumannzone/2awesome4u" target="_blank">here</a>.
-            </p>
-
-          </b-collapse>
-        </div>
-      </b-card>
+        <template v-slot:extra-code>
+          <p class="lead">
+            More info & slides <a class="font-weight-bold" href="https://github.com/baumannzone/2awesome4u" target="_blank">here</a>.
+          </p>
+        </template>
+      </ExampleBlock>
 
     </MainBlock>
   </div>
@@ -150,10 +123,12 @@ import MainBlock from '@/components/MainBlock.vue'
 import DefinitionBlock from '../../components/DefinitionBlock'
 
 import data from './data'
+import ExampleBlock from '../../components/ExampleBlock'
 
 export default {
   name: 'Speech',
   components: {
+    ExampleBlock,
     DefinitionBlock,
     MainBlock
   },
