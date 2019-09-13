913fed1936ac9b240a64fb615d84e1508d79813f
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-09-13 19:15:33 +0200

    Image capture Example 1 + click

--- src/views/ImageCapture/Example1.vue
+++ src/views/ImageCapture/Example1.vue
@@ -2,64 +2,40 @@
   <ExampleBlock exampleNumber="1">
     <template v-slot:code>
       <div class="data">
-        <h5>Audio Recorder</h5>
-        <div class="d-flex justify-content-end">
-          <b-badge v-if="supported" pill variant="success">Supported</b-badge>
-          <b-badge v-else pill variant="danger">Not supported</b-badge>
+        <div class="mb-2">
+          <b-button variant="primary" size="sm" @click="demo" class="mr-2">Capture photo</b-button>
         </div>
-        <template v-if="supported">
-          <b-button variant="primary" size="sm" @click="start" class="mr-2">Start</b-button>
-          <b-button size="sm" @click="stop">Stop</b-button>
-          <div v-if="hasAudios">
-            <hr>
-            <ul class="list-unstyled audio-list">
-              <li v-for="(item, idx) in audioList" :key="idx" class="audio-item mb-3">
-                <div class="d-flex justify-content-end">
-                  <audio controls :src="item.src" class="mr-2"></audio>
-                  <b-button variant="danger" size="sm" @click="removeItem(idx)">Delete</b-button>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </template>
+        <audio id="myVideo" hidden ref="audioPlayer">
+          <source src="../../assets/click.mp3" type="audio/mp3">
+          Your browser does not support the audio element.
+        </audio>
+        <div v-if="showImage">
+          <a :href="srcImage" title="Download Image">
+            <img :src="srcImage" width="400px" alt="img">
+          </a>
+        </div>
       </div>
     </template>
 
     <template v-slot:example-code>
       <CodeLang lang="javascript"/>
       <highlight-code lang="javascript">
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          // Supported
-        } else {
-          // Not supported
-        }
-
-        // Asking for audio only (mic)
-        navigator.mediaDevices.getUserMedia({ audio: true })
+        navigator.mediaDevices.getUserMedia({ video: true })
           .then((stream) => {
-            // stream
+            const track = stream.getVideoTracks()[0]
+            const imageCapture = new ImageCapture(track)
 
-            mediaRecorder = new MediaRecorder(stream)
+            // Capture
+            imageCapture.takePhoto()
+              .then(blob => {
+                // Our image here, easy as F*** 💘
+                imageSrc = window.URL.createObjectURL(blob)
 
-            // Save chunks
-            mediaRecorder.ondataavailable = (e) => {
-              chunks.push(e.data)
-            }
-
-            mediaRecorder.onstop = (e) => {
-              // Process chunks
-            }
-
-          })
-          .catch((err) => {
-            // err
+                // https://developer.mozilla.org/en-US/docs/Web/API/URL/revokeObjectURL
+                window.URL.revokeObjectURL(blob)
+              })
           })
 
-      </highlight-code>
-
-      <highlight-code>
-        mediaRecorder.start()
-        mediaRecorder.stop()
       </highlight-code>
     </template>
   </ExampleBlock>
@@ -77,61 +53,33 @@ export default {
   },
   data () {
     return {
-      supported: false,
-      mediaRecorder: undefined,
-      chunks: [],
-      audioList: []
+      showImage: false,
+      srcImage: undefined
     }
   },
-  created () {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      this.supported = true
-      navigator.mediaDevices.getUserMedia({ audio: true })
+  methods: {
+    demo () {
+      navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
-          console.log(stream)
+          const track = stream.getVideoTracks()[0]
+          const imageCapture = new ImageCapture(track)
 
-          this.mediaRecorder = new MediaRecorder(stream)
-
-          this.mediaRecorder.ondataavailable = (e) => {
-            this.chunks.push(e.data)
-          }
-
-          this.mediaRecorder.onstop = (e) => {
-            console.log('recorder stopped')
-
-            var blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' })
-            this.chunks = []
-            const audioURL = window.URL.createObjectURL(blob)
-
-            this.audioList.push({
-              src: audioURL
-            })
+          // Capture
+          const audio = this.$refs.audioPlayer
+          audio.currentTime = 0
+          audio.play()
+          audio.onended = (event) => {
+            imageCapture.takePhoto()
+              .then(blob => {
+                this.srcImage = window.URL.createObjectURL(blob)
+                this.showImage = true
+                window.URL.revokeObjectURL(blob)
+              })
           }
         })
         .catch((err) => {
           console.log('Err: ' + err)
         })
-    } else {
-      this.supported = false
-    }
-  },
-  computed: {
-    hasAudios () {
-      return this.audioList.length > 0
-    }
-  },
-  methods: {
-    start () {
-      this.mediaRecorder.start()
-      console.log(this.mediaRecorder.state)
-      console.log('recorder started')
-    },
-    stop () {
-      this.mediaRecorder.stop()
-      console.log(this.mediaRecorder.state)
-    },
-    removeItem (idx) {
-      this.audioList.splice(idx, 1)
     }
   }
 }
