75cc583f9e075aaaadfade974623cabfbbe33594
Josef Biehler <gabbersepp@googlemail.com>
2019-10-14 21:14:38 +0200

    extend battery test

--- cypress/specs/views/battery.spec.js
+++ cypress/specs/views/battery.spec.js
@@ -1,17 +1,18 @@
 import { battery, visitAndMock } from "../../support";
 
 describe("Battery view", () => {
-    beforeEach(() => {
+    it("should display battery information", () => {
         visitAndMock("getBattery", {
             level: 0.12,
             charging: false,
-            chargingTime: 100,
-            dischargingTime: 1000
+            chargingTime: 60,
+            dischargingTime: 120
         });
         battery();
-    });
 
-    it("should display battery information", () => {
         cy.get(".running-code li").first().should("contain", "12 %");
-    })
+        cy.get(".running-code li").eq(1).should("contain", "No");
+        cy.get(".running-code li").eq(2).should("contain", "1 min");
+        cy.get(".running-code li").eq(3).should("contain", "2 min");        
+    });
 });
